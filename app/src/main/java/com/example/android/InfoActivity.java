09cbd990b927baf48ce56cb64b596db150754f35
Ekaterina <katya070706@gmail.com>
2024-01-17 12:11:53 +0300

    Finishing for 17/01/24

--- app/src/main/java/com/example/android/InfoActivity.java
+++ app/src/main/java/com/example/android/InfoActivity.java
@@ -1,5 +1,6 @@
 package com.example.android;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -10,17 +11,43 @@ import com.example.android.databinding.ActivityInfoBinding;
 
 public class InfoActivity extends Class_Lifestyle {
     private ActivityInfoBinding binding;
-    private static final String EXTRA_TEXT_KEY = "extra_text_key";
+    private static final String EXTRA_EMAIL_KEY = "extra_email_key";
+    private static final String EXTRA_PASSWORD_KEY = "extra_password_key";
+    private static final String EXTRA_KEY_USER_KEY = "extra_password_key";
+    private static final String EXTRA_KEY_PASSW_KEY = "extra_password_key";
 
-    public static Intent createIntent(Context context, String text) {
+    public static Intent createIntent(Context context, String username, String password,
+                                      String keyUser, String keyPassw) {
         Intent intent = new Intent(context, InfoActivity.class);
-        intent.putExtra(EXTRA_TEXT_KEY, text);
+        intent.putExtra(EXTRA_PASSWORD_KEY, password);
+        intent.putExtra(EXTRA_EMAIL_KEY, username);
+        intent.putExtra(EXTRA_KEY_USER_KEY, keyUser);
+        intent.putExtra(EXTRA_KEY_PASSW_KEY, keyPassw);
         return intent;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityInfoBinding.inflate(getLayoutInflater(), null, false);
         setContentView(binding.getRoot());
+
+        String password = getIntent().getStringExtra(EXTRA_PASSWORD_KEY);
+        String email = getIntent().getStringExtra(EXTRA_EMAIL_KEY);
+        String keyUser = getIntent().getStringExtra(EXTRA_KEY_USER_KEY);
+        String keyPassw = getIntent().getStringExtra(EXTRA_KEY_PASSW_KEY);
+
+        binding.printUser.setText(email);
+        binding.printPass.setText(password);
+
+        binding.save.setOnClickListener(v -> {
+            String changedEMail = binding.printUser.getText().toString();
+            String changedPassword = binding.printPass.getText().toString();
+            Intent intent = new Intent();
+            intent.putExtra(keyUser, changedEMail);
+            intent.putExtra(keyPassw, changedPassword);
+            setResult(RESULT_OK, intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -1,19 +1,40 @@
 package com.example.android;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends Class_Lifestyle {
 
-    private static final String RESULT_KEY = "result_key";
+    private static final String PASSWORD_KEY = "result_password_key";
+    private static final String EMAIL_KEY = "result_email_key";
     private ActivityMainBinding binding;
+
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                showResultCode(result.getResultCode());
+                if (result.getData() != null) {
+                    String email = result.getData().getStringExtra(EMAIL_KEY);
+                    String password = result.getData().getStringExtra(PASSWORD_KEY);
+                    binding.username.setText(email);
+                    binding.username.clearFocus(); // Don't open keyboard
+
+                    binding.password.setText(password);
+                    binding.password.clearFocus(); // Don't open keyboard
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +42,26 @@ public class MainActivity extends Class_Lifestyle {
         binding = ActivityMainBinding.inflate(getLayoutInflater(), null, false);
         setContentView(R.layout.activity_main);
 
-        TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
-
         binding.loginbtn.setOnClickListener(v -> {
-            Intent intent= new Intent(this, InfoActivity.class);
-            startActivity(intent);
+            String email = binding.username.getText().toString();
+            String password = binding.password.getText().toString();
+            Intent intent = InfoActivity.createIntent(this, email, password, PASSWORD_KEY,
+                    EMAIL_KEY);
+            activityResultLauncher.launch(intent);
         });
+    }
+
+    private void showResultCode(int resultCode) {
+        if (resultCode == RESULT_OK) {
+            showToast("Зарегистрирован");
+        } else if (resultCode == RESULT_CANCELED) {
+            showToast("RESULT_CANCELED");
+        } else {
+            showToast(String.valueOf(resultCode));
+        }
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
